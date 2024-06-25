3cf01c092a731312405e607e331d13cfb3032130
sherikbro <sherzodomonov8776@gmail.com>
2024-06-25 18:56:08 +0500

    main

---
+++ main.js
@@ -0,0 +1,10 @@
+const sd=document.querySelector(".navbar");
+const bar=document.querySelector(".burger");
+bar.addEventListener('click', ()=>{
+    sd.classList.toggle('active');
+});
+
+
+const save =()=>{
+    sd.classList.toggle('active');
+}
